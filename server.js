83c1eb90088fd94886057b6a022fc47f18abd345
Jacastro619 <jorgecastro619@gmail.com>
2023-10-25 16:50:06 -0700

    added psuedocode to server.js

--- server.js
+++ server.js
@@ -25,3 +25,21 @@ app.get("/api/notes", (req, res) => res.json(db));
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
 });
+
+// import modules required
+// set up app variable to a new express object
+// create middleware urlencoded
+// create middleware json
+// create middleware static
+// write a get, post and delete end point routes for notes that are fetched in index.js
+// GET hmtl route needs to send the index.html file
+// GET notes route to send the notes.html
+// GET request for api/notes to return the db.json file
+// POST request to post into db.json file
+// push the parsed body data into the array
+// object destructing the req.body
+// create new object with new variables from req.body
+// writeFile the new array
+// Delete request to delete specific note by id /api/notes/id
+// get requests
+// app listen
