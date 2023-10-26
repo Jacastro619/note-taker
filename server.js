e6bc823c5ff46d8bf032dc00521514f1a7b6ab7f
Jacastro619 <jorgecastro619@gmail.com>
2023-10-25 17:01:09 -0700

    added POST method to api/notes url to capture input from index.js file

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
@@ -21,6 +20,23 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => res.json(db));
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  const newNote = {
+    title,
+    text,
+  };
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+  console.log(response);
+});
 
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
